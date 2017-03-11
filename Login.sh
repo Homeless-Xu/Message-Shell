0a7af33c4c265edbf4b7f4e35bf06ef1c69ae718
Xu Jian <xx2610@gmail.com>
2017-03-11 09:24:33 +0800

    Update

--- Login.sh
+++ Login.sh
@@ -1,14 +1,5 @@
 #!/bin/sh
-
-# 这个是 登录电脑 就发送短信的脚本.
-
-# 把这个脚本 加入到苹果自带的命令就可以开机启动(这种方法只支持一个脚本)
-# sudo defaults write com.apple.loginwindow LoginHook '/Users/v/Desktop/php/message.php'
-
-# 查看现有的 开机脚本
-# sudo defaults read com.apple.loginwindow LoginHook
-# 删除现有的 开机脚本
-# sudo defaults delete com.apple.loginwindow LoginHook
-
-
 php /Users/v/Desktop/php/message.php
+
+
+#这里只要把php 后面的路径. 换成你php脚本的路径就可以了
